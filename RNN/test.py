f06efd38508e7a5278b4d1003a590db359cb2a28
Lokesh Sharma <63437596+lokesharma-dev@users.noreply.github.com>
2020-05-06 10:00:20 +0200

    Update test.py

--- RNN/test.py
+++ RNN/test.py
@@ -1,38 +1,44 @@
 import pandas as pd
-
-df = pd.read_csv("indian_liver_patient.csv")
-
-df.nunique()
-df.head()
-
-
-import pandas as pd
 import numpy as np
-
-
-import warnings
-warnings.filterwarnings('ignore')
-np.random.seed(0)
-
-
-import seaborn as sns
 import matplotlib.pyplot as plt
 
 
-plt.figure(figsize=(10,6))
-sns.heatmap(df.corr(),cmap='magma',annot=True)
+df = pd.read_csv("indian_liver_patient.csv")
+
+# # instantiate the figure
+# fig = plt.figure(figsize = (10, 5))
+# gs = fig.add_gridspec(5, 5)
+# ax1 = fig.add_subplot(gs[:4, :-1])
+#
+# x, y = df['Alkaline_Phosphotase'], df['Age']
+#
+# ax1.scatter(x, y, c = df.Dataset.astype('category').cat.codes)
+# ax1.set_xlabel("Dist")
+# ax1.set_ylabel("Hwy")
+# ax1.set_title("Scatter plot with marginal histograms")
+#
+# # Removes the boundaries
+# ax1.spines["right"].set_color("None")
+# ax1.spines["top"].set_color("None")
+#
+#
+# ax2 = fig.add_subplot(gs[4:, :-1])
+# ax2.hist(x, 40, orientation = 'vertical', color = "blue")
+# ax2.axison = False
+#
+# ax3 = fig.add_subplot(gs[:4, -1])
+# ax3.hist(y, 40, orientation = "horizontal", color = "blue")
+# ax3.axison = False
+#
+# plt.show()
+
+# instanciate the figure
+fig = plt.figure(figsize = (10, 5))
+ax = fig.add_subplot()
+
+# plot using matplotlib
+ax.imshow(df.corr(), cmap = 'viridis', interpolation = 'nearest')
+# set the title for the figure
+ax.set_title("Heatmap using matplotlib")
+
 plt.show()
-
-
-corr = df.corr()
-ax = sns.heatmap(
-    corr,
-    vmin=-1, vmax=1, center=0,
-    cmap=sns.diverging_palette(20, 220, n=200),
-    square=True
-)
-ax.set_xticklabels(
-    ax.get_xticklabels(),
-    rotation=45,
-    horizontalalignment='right'
-);
